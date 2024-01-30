021157489596d7388a4133bf5d94b1522fcf7a38
22JAllard <155989473+22JAllard@users.noreply.github.com>
2024-01-30 09:46:56 +0000

    step 1

---
+++ 4pong 01.py
@@ -0,0 +1,19 @@
+import pygame #import pygame
+import sys #imports system
+
+width = 500
+height = 500
+fps=60
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Pong')
+
+run = True
+while run:
+    screen.fill(black)
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
